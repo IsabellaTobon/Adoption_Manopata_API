3187ef8106536e7a71f81c0c41a4c851a28ffd41
Isabella Tobón Rodríguez <89875811+IsabellaTobon@users.noreply.github.com>
2024-08-15 13:51:02 +0200

    creados todos los models

--- src/main/java/com/example/adoption_Manopata/model/Post.java
+++ src/main/java/com/example/adoption_Manopata/model/Post.java
@@ -1,4 +1,37 @@
 package com.example.adoption_Manopata.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String photo;
+    private Date registerDate;
+    private String name;
+    private Integer age;
+    private String animalType;
+    private Boolean vaccinate;
+    private String breed;
+    private Boolean ppp;
+    private String city;
+    private String province;
+    private Boolean available;
+    private Integer likes = 0;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
 }
