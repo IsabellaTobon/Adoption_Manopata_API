d139f31da588704169ffddcd4a893306d6c56e5c
Isabella Tobón Rodríguez <89875811+IsabellaTobon@users.noreply.github.com>
2024-10-12 23:19:02 +0200

    cambio de  dependencias

--- src/main/java/com/example/adoption_Manopata/model/Comment.java
+++ src/main/java/com/example/adoption_Manopata/model/Comment.java
@@ -1,9 +1,9 @@
 package com.example.adoption_Manopata.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import jakarta.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 @Entity
